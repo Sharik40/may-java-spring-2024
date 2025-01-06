813067ed1b4a041dbd08fe977db4066603f7def6
Sharik40 <sharik201620152014@gmail.com>
2025-01-06 23:04:37 +0200

    Initial commit

---
+++ src/main/java/org/example/mayjavaspring2024/MayJavaSpring2024Application.java
@@ -0,0 +1,13 @@
+package org.example.mayjavaspring2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MayJavaSpring2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MayJavaSpring2024Application.class, args);
+	}
+
+}
